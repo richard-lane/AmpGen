80c4da8b3316864ced5c1fad13df28949796a723
Richard Lane <richard.lane@bristol.ac.uk>
2019-11-07 14:43:26 +0000

    refactor binning file to be a bit more streamlined

--- binning/bin_generated_decays.cpp
+++ binning/bin_generated_decays.cpp
@@ -4,8 +4,9 @@
  *
  * NOTE: if this fails to build with error "dcs.so: No such file or directory", try restarting ROOT and building again.
  *
- * This script is gross and needs a lot of clearing up; only exists to test my understanding
+ * This script contains no error handling, arg verification or anything to make it work nicely
  */
+
 #include <cmath>
 #include <complex>
 #include <cstring>
@@ -24,6 +25,9 @@
 #include "k3pi_binning.h"
 
 // ---- Magic Numbers
+// Max length of a branch name in the ROOT file of interest
+#define NAME_LENGTH 10
+
 // DCS and CF relative amplitude and phase
 #define DCS_MAGNITUDE 0.0601387
 #define DCS_PHASE 1.04827
@@ -31,12 +35,6 @@
 /// Bin limits in phase, centred on zero by construction
 #define NUM_BINS 5
 #define BIN_LIMITS -39, 0, 43, 180
-
-inline bool fileExists(const std::string &name)
-{
-    ifstream f(name.c_str());
-    return f.good();
-}
 
 /*
  * CoM energy squared of the ab system
@@ -64,40 +62,27 @@ double *s(double **particleA, double **particleB, const unsigned int length)
 }
 
 /*
- * Plot CoM energies s01 vs s02
+ * Plot the K energies and make a plot of s01 vs s02 to check consistency with the ROOT TBrowser
  *
  */
-void plotS01VsS02(const char *title, double **kArrays, double **pi1Arrays, double **pi2Arrays, size_t length)
+void plot_things(double **kArrays, double **pi1Arrays, double **pi2Arrays, size_t length)
 {
-    const double *s01Values{nullptr};
-    const double *s02Values{nullptr};
 
-    s01Values = s(kArrays, pi1Arrays, length);
-    s02Values = s(kArrays, pi2Arrays, length);
+    // Plot K energies
+    auto  kCanvas = new TCanvas("K energies", "K energies", 600, 600);
+    TH1D *hist    = new TH1D("K energies", "K energies", 100, 0.45, 1);
+    hist->FillN(length, kArrays[3], 0);
+    hist->Draw();
 
-    auto    myCanvas = new TCanvas("CoM Energies", "CoM Energies", 600, 600);
-    TGraph *myGraph  = new TGraph(length, s01Values, s02Values);
+    // Plot CoM energies on a new canvas
+    auto    comCanvas = new TCanvas("CoM Energies", "CoM Energies", 600, 600);
+    double *s01       = s(kArrays, pi1Arrays, length);
+    double *s02       = s(kArrays, pi2Arrays, length);
+    TGraph *myGraph   = new TGraph(length, s(kArrays, pi1Arrays, length), s(kArrays, pi2Arrays, length));
     myGraph->Draw("AP");
 
-    delete s01Values;
-    delete s02Values;
-}
-
-/*
- * Plot an array
- */
-void plotArray(const char *  title,
-               const double *array,
-               size_t        length,
-               size_t        nBins = 100,
-               double        xMin  = 0.0,
-               double        xMax  = 1.0)
-{
-    auto  myCanvas = new TCanvas(title, title, 600, 600);
-    TH1D *hist     = new TH1D(title, title, nBins, xMin, xMax);
-
-    hist->FillN(length, array, 0);
-    hist->Draw();
+    delete[] s01;
+    delete[] s02;
 }
 
 /*
@@ -118,8 +103,51 @@ void writeArray(TTree *myTree, const char *myBranchName, double *myArray)
     }
 
     // Reset all branch addresses to avoid a bug where repeatedly calling this function would set an array to the wrong
-    // value
+    // values
     myTree->ResetBranchAddresses();
+}
+
+/*
+ * Write an array of arrays for storing particle data
+ *
+ * Allocates memory for the data arrays which must be freed by the caller.
+ * Also allocates memory to the array of arrays which must be freed
+ */
+double **writeArrays(TTree *myTree, const char *name, size_t length)
+{
+
+    // Generate names for the arrays
+    // This is a stupid way to initialise an array but i can't think of a better one
+    char xArrayName[NAME_LENGTH]{
+        name[0], name[1], name[2], name[3], name[4], name[5], name[6], name[7], name[8], name[9]};
+    char yArrayName[NAME_LENGTH]{
+        name[0], name[1], name[2], name[3], name[4], name[5], name[6], name[7], name[8], name[9]};
+    char zArrayName[NAME_LENGTH]{
+        name[0], name[1], name[2], name[3], name[4], name[5], name[6], name[7], name[8], name[9]};
+    char eArrayName[NAME_LENGTH]{
+        name[0], name[1], name[2], name[3], name[4], name[5], name[6], name[7], name[8], name[9]};
+
+    strcat(xArrayName, "_Px");
+    strcat(yArrayName, "_Py");
+    strcat(zArrayName, "_Pz");
+    strcat(eArrayName, "_E");
+
+    // Allocate memory for data arrays
+    double *const xArray = new double[length];
+    double *const yArray = new double[length];
+    double *const zArray = new double[length];
+    double *const eArray = new double[length];
+
+    // Create an array of arrays
+    double **arrays = new double *[4] { xArray, yArray, zArray, eArray };
+
+    // Write the data to the arrays
+    writeArray(myTree, xArrayName, xArray);
+    writeArray(myTree, yArrayName, yArray);
+    writeArray(myTree, zArrayName, zArray);
+    writeArray(myTree, eArrayName, eArray);
+
+    return arrays;
 }
 
 /*
@@ -128,26 +156,6 @@ void writeArray(TTree *myTree, const char *myBranchName, double *myArray)
  */
 void bin_generated_decays(TFile *inputFile)
 {
-    // Apply scaling and rotation to the DCS amplitude such that we get dcs/cf amplitude ratio 'r' = 0.055
-    // and the average relative strong-phase between the two amplitudes ~ 0.
-    std::complex<double> dcs_offset = DCS_MAGNITUDE * exp(std::complex<double>(0, 1) * DCS_PHASE * M_PI / 180.);
-
-    // Define the bins based on the form of the DCS and CF decays
-    const std::string dcsFile{"binning/dcs.so"};
-    const std::string cfFile{"binning/cf.so"};
-
-    if (!fileExists(dcsFile)) {
-        std::cout << "File '" << dcsFile << "' not found." << std::endl;
-        throw 1;
-    }
-    if (!fileExists(cfFile)) {
-        std::cout << "File '" << cfFile << "' not found." << std::endl;
-        throw 1;
-    }
-
-    // Create bins based on DCS and CF amplitudes
-    k3pi_binning::binning bins(dcsFile, cfFile, dcs_offset, {BIN_LIMITS});
-
     // Read in the tree and branches from the provided ROOT file
     // The tree of interest is DalitzEventList as this contains the decay products' kinematic data
     TTree *myTree = nullptr;
@@ -156,54 +164,20 @@ void bin_generated_decays(TFile *inputFile)
     // Number of datapoints
     const unsigned int length = myTree->GetEntries();
 
-    // Create an array of arrays pointing to the K data
-    // Lots of reused code here but the effort to refactor won't be worth the trouble
-    double *const kPxArray   = new double[length];
-    double *const kPyArray   = new double[length];
-    double *const kPzArray   = new double[length];
-    double *const kEArray    = new double[length];
-    double *kArrays[4] = {kPxArray, kPyArray, kPzArray, kEArray};
+    // Create an arrays of arrays pointing to the particle data
+    double **kArrays   = writeArrays(myTree, "_1_K~", length);
+    double **pi1Arrays = writeArrays(myTree, "_2_pi#", length);
+    double **pi2Arrays = writeArrays(myTree, "_3_pi#", length);
+    double **pi3Arrays = writeArrays(myTree, "_4_pi~", length);
 
-    writeArray(myTree, "_1_K~_E", kEArray);
-    writeArray(myTree, "_1_K~_Px", kPxArray);
-    writeArray(myTree, "_1_K~_Py", kPyArray);
-    writeArray(myTree, "_1_K~_Pz", kPzArray);
+    // Apply scaling and rotation to the DCS amplitude such that we get dcs/cf amplitude ratio 'r' = 0.055
+    // and the average relative strong-phase between the two amplitudes ~ 0.
+    std::complex<double> dcs_offset = DCS_MAGNITUDE * exp(std::complex<double>(0, 1) * DCS_PHASE * M_PI / 180.);
 
-    // Create arrays pointing to the pi- data
-    double *const pi1PxArray   = new double[length];
-    double *const pi1PyArray   = new double[length];
-    double *const pi1PzArray   = new double[length];
-    double *const pi1EArray    = new double[length];
-    double *pi1Arrays[4] = {pi1PxArray, pi1PyArray, pi1PzArray, pi1EArray};
-
-    double *const pi2PxArray   = new double[length];
-    double *const pi2PyArray   = new double[length];
-    double *const pi2PzArray   = new double[length];
-    double *const pi2EArray    = new double[length];
-    double *pi2Arrays[4] = {pi2PxArray, pi2PyArray, pi2PzArray, pi2EArray};
-
-    writeArray(myTree, "_2_pi#_E", pi1EArray);
-    writeArray(myTree, "_2_pi#_Px", pi1PxArray);
-    writeArray(myTree, "_2_pi#_Py", pi1PyArray);
-    writeArray(myTree, "_2_pi#_Pz", pi1PzArray);
-
-    writeArray(myTree, "_3_pi#_E", pi2EArray);
-    writeArray(myTree, "_3_pi#_Px", pi2PxArray);
-    writeArray(myTree, "_3_pi#_Py", pi2PyArray);
-    writeArray(myTree, "_3_pi#_Pz", pi2PzArray);
-
-    // Create arrays for pi+ data
-    double *const pi3PxArray   = new double[length];
-    double *const pi3PyArray   = new double[length];
-    double *const pi3PzArray   = new double[length];
-    double *const pi3EArray    = new double[length];
-    double *pi3Arrays[4] = {pi3PxArray, pi3PyArray, pi3PzArray, pi3EArray};
-
-    writeArray(myTree, "_4_pi~_E", pi3EArray);
-    writeArray(myTree, "_4_pi~_Px", pi3PxArray);
-    writeArray(myTree, "_4_pi~_Py", pi3PyArray);
-    writeArray(myTree, "_4_pi~_Pz", pi3PzArray);
-
+    // Define the bins based on the form of the DCS and CF decays
+    const std::string     dcsFile{"binning/dcs.so"};
+    const std::string     cfFile{"binning/cf.so"};
+    k3pi_binning::binning bins(dcsFile, cfFile, dcs_offset, {BIN_LIMITS});
 
     /// initialise global hadronic parameters, and parameters in each of the bins.
     std::complex<double>              z(0, 0);
@@ -214,17 +188,16 @@ void bin_generated_decays(TFile *inputFile)
     std::vector<double>               n_dcs_binned(NUM_BINS, 0);
 
     for (int i = 0; i < myTree->GetEntries(); ++i) {
+        // Create a vector of TLorentzVectors for this event (K+, pi-, pi-, pi+)
         TLorentzVector kLorentzVector{kArrays[0][i], kArrays[1][i], kArrays[2][i], kArrays[3][i]};
         TLorentzVector pi1LorentzVector{pi1Arrays[0][i], pi1Arrays[1][i], pi1Arrays[2][i], pi1Arrays[3][i]};
         TLorentzVector pi2LorentzVector{pi2Arrays[0][i], pi2Arrays[1][i], pi2Arrays[2][i], pi2Arrays[3][i]};
         TLorentzVector pi3LorentzVector{pi3Arrays[0][i], pi3Arrays[1][i], pi3Arrays[2][i], pi3Arrays[3][i]};
 
-        // @@@ todo:
-        //      Create a vector of TLorentzVectors for this event (K+, pi-, pi-, pi+)
         std::vector<TLorentzVector> eventVector{kLorentzVector, pi1LorentzVector, pi2LorentzVector, pi3LorentzVector};
-        auto event = k3pi_binning::eventFromVectors(eventVector);
+        auto                        event = k3pi_binning::eventFromVectors(eventVector);
 
-        //      @@@ Work out the CF and DCS amplitudes of this event, using the bins object created above
+        // Work out the CF and DCS amplitudes of this event, using the bins object created above
         auto eval_cf  = bins.cf(event.data(), 1);
         auto eval_dcs = dcs_offset * bins.dcs(event.data(), 1);
 
@@ -238,22 +211,16 @@ void bin_generated_decays(TFile *inputFile)
         z_binned[bin] += eval_cf * std::conj(eval_dcs);
         n_cf_binned[bin] += std::norm(eval_cf);
         n_dcs_binned[bin] += std::norm(eval_dcs);
-
     }
 
-    //double *s01Values{nullptr};
-    //double *s02Values{nullptr};
-    //s01Values = s(kArrays, pi1Arrays, length);
-    //s02Values = s(kArrays, pi2Arrays, length);
-
-    //plotArray("K energies", kArrays[3], length);
-    //plotS01VsS02("S01 vs S02", kArrays, pi1Arrays, pi2Arrays, length);
+    // Make some plots to check that the data from ROOT has been read in correctly
+    plot_things(kArrays, pi1Arrays, pi2Arrays, length);
 
     // Output hadronic parameters
     std::cout << "==== Global: =====================" << std::endl;
-    std::cout << "R = " << std::abs(z) / sqrt(n_cf * n_dcs) << std::endl; /// should be ~ 0.47
-    std::cout << "d = " << std::arg(z) * 180 / M_PI << std::endl; /// should be ~ zero by construction (< 1 degree)
-    std::cout << "r = " << sqrt(n_dcs / n_cf) << std::endl;       /// should be ~ 0.055
+    std::cout << "R = " << std::abs(z) / sqrt(n_cf * n_dcs) << "\t// Should be ~0.47" << std::endl;
+    std::cout << "d = " << std::arg(z) * 180 / M_PI << "\t// Should be ~ zero by construction (< 1 degree)" << std::endl;
+    std::cout << "r = " << sqrt(n_dcs / n_cf) << "\t// Should be ~ 0.055" << std::endl;
 
     for (int i = 0; i < NUM_BINS; ++i) {
         std::cout << "==== Bin " << i + 1 << ": ======================" << std::endl;
@@ -265,7 +232,6 @@ void bin_generated_decays(TFile *inputFile)
     }
     std::cout << "==================================" << std::endl;
 
-
     // free arrays
     for (int i = 0; i < 4; ++i) {
         delete[] kArrays[i];
@@ -273,4 +239,8 @@ void bin_generated_decays(TFile *inputFile)
         delete[] pi2Arrays[i];
         delete[] pi3Arrays[i];
     }
+    delete[] kArrays;
+    delete[] pi1Arrays;
+    delete[] pi2Arrays;
+    delete[] pi3Arrays;
 }
