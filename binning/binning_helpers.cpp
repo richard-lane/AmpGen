459cfa0df77b835b615cfcc096e05e5d369d8b23
Richard Lane <richard.lane@bristol.ac.uk>
2019-12-11 16:30:23 +0000

    better implementation of the vector splitting into bins based on limits

--- binning/binning_helpers.cpp
+++ binning/binning_helpers.cpp
@@ -11,7 +11,9 @@
 #include <vector>
 
 /*
- * Split a (sorted) vector based on binLimits
+ * Split a (sorted) vector based on binLimits (it wouldn't be hard to make this work with unsorted vectors, though it's
+ * slightly more efficient if we demand that vectors are sorted)
+ *
  * Bin limits define lowest, highest and intermediate points
  * Bin limits should be sorted
  */
@@ -19,26 +21,45 @@ std::vector<std::vector<double>> splitVectorWithLimits(std::vector<double> &myVe
 {
     // Bin limits should be sorted
     if (!std::is_sorted(binLimits.begin(), binLimits.end())) {
-        std::cout << "Bad time bin limits; should be sorted" << std::endl;
+        std::cerr << "Bad time bin limits; should be sorted" << std::endl;
         throw;
     }
 
     // Vector should be sorted
     if (!std::is_sorted(myVector.begin(), myVector.end())) {
-        std::cout << "Vector must be sorted in order to split" << std::endl;
+        std::cerr << "Vector must be sorted in order to split" << std::endl;
         throw;
     }
 
-    // TODO create a consistent definition of what numbins are
-    size_t                           numBins = binLimits.size() - 1;
+    // First point in the vector should be in a bin; if it is smaller than the lowest bin emit a warning
+    if (myVector[0] < binLimits[0]) {
+        std::cerr << "[warning] Vector contains values smaller than lowest bin limit" << std::endl;
+    }
+
+    // Last point in the vector should be in a bin; throw if it is larger than the highest bin
+    size_t vectorSize = myVector.size();
+    size_t numBins    = binLimits.size() - 1;
+    if (myVector[vectorSize - 1] > binLimits[numBins]) {
+        std::cerr << "Vector extends over range larger than largest bin limit" << std::endl;
+        throw;
+    }
+
+    // Split our vector into smaller vectors along the bin limits
     std::vector<std::vector<double>> splitVector(numBins);
 
+    // Left edge of bin we are currently inserting points into
     size_t currentTimeBin = 0;
-    for (size_t i = 0; i < myVector.size(); ++i) {
+    for (size_t i = 0; i < vectorSize; ++i) {
         // If this time is more than the bin limit, we want to put subsequent points in a higher bin
         // Unless we are already putting points in the highest bin
-        while (myVector[i] > binLimits[currentTimeBin] && currentTimeBin < numBins - 1) {
+        while (myVector[i] > binLimits[currentTimeBin + 1] && currentTimeBin < numBins - 1) {
             currentTimeBin += 1;
+
+            // I think this code never gets called...?
+            if (currentTimeBin > numBins) {
+                std::cerr << "Attempted to insert points into a bin that does not exist" << std::endl;
+                throw;
+            }
         }
         splitVector[currentTimeBin].push_back(myVector[i]);
     }
